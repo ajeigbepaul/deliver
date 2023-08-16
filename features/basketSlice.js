dad299cd028cf5927bbdfd08a282068d6abf8a97
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-08-16 16:44:57 +0100

    introduce redux

---
+++ features/basketSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+export const basketSlice = createSlice({
+  name: "basket",
+  initialState,
+  reducers: {
+    addToBasket: (state,action) => {
+    },
+    removeFromBasket: (state,action) => {
+    }
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+export default counterSlice.reducer;
