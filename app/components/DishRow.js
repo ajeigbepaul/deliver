00f4377e9dedb87ec76f63f91d0049c76ad87b97
ajeigbepaul <ajeigbepaul@yahoo.com>
2023-08-17 12:29:19 +0100

    implemented the redux for the items

--- app/components/DishRow.js
+++ app/components/DishRow.js
@@ -1,23 +1,40 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React, { useState } from "react";
-// import Currency from "react-currency-formatter";
+
 import { urlFor } from "../../sanity";
 import { MinusCircleIcon, PlusCircleIcon } from "react-native-heroicons/solid";
+import { useDispatch, useSelector} from "react-redux";
+import {
+  addToBasket,
+  removeFromBasket,
+  selectBasketItemswithId,
+} from "../../features/basketSlice";
 const DishRow = ({ id, name, description, price, image }) => {
+  const items = useSelector((state) => selectBasketItemswithId(state, id));
+  const dispatch = useDispatch();
+  const handleAddtoBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }));
+  };
+  const handleRemoveitem = ()=>{
+    if(!items?.length > 0) return;
+    dispatch(removeFromBasket({id}))
+  }
+  let naira = Intl.NumberFormat("en-NG");
   const [ispressed, setIspressed] = useState(false);
   return (
     <>
       <TouchableOpacity
-        className={`bg-white border p-4 border-gray-200 ${ispressed && "border-b-0"}`}
+        className={`bg-white border p-4 border-gray-200 ${
+          ispressed && "border-b-0"
+        }`}
         onPress={() => setIspressed(!ispressed)}
       >
         <View className="flex-row">
           <View className="flex-1 pr-2">
             <Text className="text-lg mb-1">{name}</Text>
             <Text className="text-gray-400">{description}</Text>
-            <Text className="text-gray-400 mt-2">
-              {/* <Currency quantity={price} currency="NGN" /> */}
-            </Text>
+
+            <Text className="text-gray-400 mt-2">₦ {naira.format(price)}</Text>
           </View>
           <View>
             <Image
@@ -31,12 +48,18 @@ const DishRow = ({ id, name, description, price, image }) => {
       {ispressed && (
         <View className="p-4 bg-white">
           <View className="flex-row items-center space-x-2 mb-3">
-            <TouchableOpacity>
-              <MinusCircleIcon color="dodgerblue" size={30} />
+            <TouchableOpacity
+              onPress={handleRemoveitem}
+              disabled={!items.length}
+            >
+              <MinusCircleIcon
+                color={items.length > 0 ? "orange" : "gray"}
+                size={40}
+              />
             </TouchableOpacity>
-            <Text>0</Text>
-            <TouchableOpacity>
-              <PlusCircleIcon color="dodgerblue" size={30} />
+            <Text>{items.length}</Text>
+            <TouchableOpacity onPress={handleAddtoBasket}>
+              <PlusCircleIcon color="orange" size={50} />
             </TouchableOpacity>
           </View>
         </View>
